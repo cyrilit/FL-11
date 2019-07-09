40a708a74403f490dddd91f37820f45cfc7e81ee
Kyrylo Anufriienko <cyrility@gmail.com>
2019-07-09 23:56:42 +0300

    task hw7 2/2 done

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -5,33 +5,34 @@ const resources = {
 	factor: 1,
 	total: 0,
 	basicPrizes: {
-		0: 100, 
-		1: 50, 
-		2: 25
+		3: 100, 
+		2: 50, 
+		1: 25
 	}
 };
 
 if ( confirm('Do you want to play a game?') ) {
-	for (let i = 0; i < resources.attempts; i++) {		
+	for (; resources.attempts > 0; resources.attempts--) {		
 		let ballLand = Math.floor( Math.random() * (resources.numBound + 1) );
 		let choice = parseInt( prompt(
 					`Chose a roulette pocket number from 0 to ${resources.numBound}
-					Attempts left: ${resources.attempts - i}
+					Attempts left: ${resources.attempts}
 					Total prize: ${resources.total}$
-					Possible prize on current attempt: ${resources.basicPrizes[i] * resources.factor}$`, ''), 10 );
+					Possible prize on current attempt: 
+					${resources.basicPrizes[resources.attempts] * resources.factor}$`, ''), 10 );
 		if (choice === ballLand) {
-			resources.total += resources.basicPrizes[i] * resources.factor;
+			resources.total += resources.basicPrizes[resources.attempts] * resources.factor;
 			if ( confirm(`Congratulation, you won!   Your prize is: ${resources.total} $. Do you want to continue?`) ) {
 				resources.numBound += resources.expander; // make number range bigger at 4 as the previous one
 				resources.factor *= 2; // make maximum prize two times bigger
-				i = 0; // set number of attempts to 3 
+				resources.attempts = 3; // set number of attempts to 3 
 			} else {
 				alert(`Thank you for your participation. Your prize is: ${resources.total} $`);
 				if ( confirm('Do you want to play again?') ) {
 					resources.numBound = 8;
 					resources.factor = 1;
 					resources.total = 0;
-					i = 0;
+					resources.attempts = 3;
 				} 
 			}
 		} else {
@@ -40,7 +41,7 @@ if ( confirm('Do you want to play a game?') ) {
 					resources.numBound = 8;
 					resources.factor = 1;
 					resources.total = 0;
-					i = 0;					
+					resources.attempts = 3;					
 				}
 		}
 	}
