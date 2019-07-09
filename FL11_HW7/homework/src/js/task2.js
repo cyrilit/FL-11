b6b12ab62306b61fee4f81acfd22d4168aaca509
Kyrylo Anufriienko <cyrility@gmail.com>
2019-07-09 23:19:29 +0300

    Fix release 2/2 hw7

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -14,22 +14,23 @@ const resources = {
 if ( confirm('Do you want to play a game?') ) {
 	for (let i = 0; i < resources.attempts; i++) {		
 		let ballLand = Math.floor( Math.random() * (resources.numBound + 1) );
-		let pocketNumberGuess = parseInt( prompt(
+		let choice = parseInt( prompt(
 					`Chose a roulette pocket number from 0 to ${resources.numBound}
 					Attempts left: ${resources.attempts - i}
 					Total prize: ${resources.total}$
-					Possible prize on current attempt: ${resources.basicPrizes[i]}$`, ''), 10 );
-		if (pocketNumberGuess === ballLand) {
+					Possible prize on current attempt: ${resources.basicPrizes[i] * resources.factor}$`, ''), 10 );
+		if (choice === ballLand) {
 			resources.total += resources.basicPrizes[i] * resources.factor;
 			if ( confirm(`Congratulation, you won!   Your prize is: ${resources.total} $. Do you want to continue?`) ) {
-				resources.numBound += resources.expander;
-				resources.factor *= 2;
+				resources.numBound += resources.expander; // make number range bigger at 4 as the previous one
+				resources.factor *= 2; // make maximum prize two times bigger
 				i = 0; // set number of attempts to 3 
 			} else {
 				alert(`Thank you for your participation. Your prize is: ${resources.total} $`);
 				if ( confirm('Do you want to play again?') ) {
 					resources.numBound = 8;
 					resources.factor = 1;
+					resources.total = 0;
 					i = 0;
 				} 
 			}
@@ -38,6 +39,7 @@ if ( confirm('Do you want to play a game?') ) {
 			if ( confirm('Do you want to play again?') ) {
 					resources.numBound = 8;
 					resources.factor = 1;
+					resources.total = 0;
 					i = 0;					
 				}
 		}
@@ -45,7 +47,3 @@ if ( confirm('Do you want to play a game?') ) {
 } else {
 	alert('You did not become a billionaire, but can.');
 }
-
-
-
-
