2d9dbc13cba65bdaa7bcae385d72a60ab345d264
Kyrylo Anufriienko <cyrility@gmail.com>
2019-07-14 23:56:20 +0300

    Update getMin with own logic realization

--- FL11_HW8/homework/js/getMin.js
+++ FL11_HW8/homework/js/getMin.js
@@ -1,4 +1,10 @@
 function getMin () {
-	return Math.min(...arguments);
+	let min = arguments[0];
+	for (let i = 1; i < arguments.length; i++) {
+		if (min > arguments[i]) {
+			min = arguments[i];
+		}
+	}
+	return min;
 }
 getMin(7, 0, 2, -144);
